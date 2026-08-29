e96be4002a3c64147791158fd3c3336f05d9c2e3
manthan3747 <manthandobariya64@gmail.com>
2026-08-29 09:23:06 +0530

    Time: 4 ms (40.76%) | Memory: 11.6 MB (32.43%) - LeetSync

---
+++ 649-dota2-senate/dota2-senate.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> r;
+        queue<int> d;
+
+        int n = senate.size();
+
+        for (int i = 0; i < n; i++) {
+            if (senate[i] == 'R')
+                r.push(i);
+            else
+                d.push(i);
+        }
+
+        while (!r.empty() && !d.empty()) {
+            int ri = r.front();
+            int di = d.front();
+
+            r.pop();
+            d.pop();
+
+            if (ri < di)
+                r.push(ri + n);
+            else
+                d.push(di + n);
+        }
+
+        if (r.empty())
+            return "Dire";
+
+        return "Radiant";
+    }
+};
